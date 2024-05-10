5df536b62e0e6e9239f208a46a3d8124b8658204
aaaa777 <aaaa777@users.noreply.github.com>
2024-05-10 23:51:53 +0900

    swap機能を作成

--- src/app/test1/page.js
+++ src/app/test1/page.js
@@ -3,47 +3,76 @@
 import SampleNode1 from "@/components/slide-parts/sample-node1";
 import TimelinePlayer from "@/lib/timeline-player";
 import SlidePlayer from "@/lib/slide-player";
+import AnimeManager from "@/lib/anime-manager";
 import Image from "next/image";
 import anime from 'animejs/lib/anime.es.js';
 import { useEffect, useId, useState } from "react";
 
+// TODO: 連打した時の対策
+
 export default function Home() {
 
-  let tlp = new TimelinePlayer();
-  let sp = new SlidePlayer();
+  const animeSequence = [
+    { "action": "breakpoint" },
+    { "action": "swap", "selectorA": ".sample-node .test1", "selectorB": ".sample-node .test3" },
+    { "action": "breakpoint" },
+    { "action": "swap", "selectorA": ".sample-node .test1", "selectorB": ".sample-node .test2" },
+    { "action": "breakpoint" },
+    { "action": "swap", "selectorA": ".sample-node .test1", "selectorB": ".sample-node .test4" },
+    { "action": "breakpoint" },
+    { "action": "swap", "selectorA": ".sample-node .test1", "selectorB": ".sample-node .test4" },
+    { "action": "swap", "selectorA": ".sample-node .test1", "selectorB": ".sample-node .test4" },
+    { "action": "breakpoint" },
+  ];
   let inited = false;
-
+  
   let [slideNumber, setSlideNumber] = useState(0);
   let [pressButtonCount, setPressButtonCount] = useState(0);
+  
+  // https://stackoverflow.com/questions/73149606/why-is-my-usestate-variable-initialized-every-time-my-react-components-is-render
+  const createAM = () => new AnimeManager(animeSequence);
+  let [am, setAM] = useState(() => createAM());
 
   const pressNext = () => {
-    tlp.next();
-    setPressButtonCount(pressButtonCount + 1);
+    if(am.hasNext()) {
+      setSlideNumber(slideNumber + 1);
+    }
+    am.next();
+    // setPressButtonCount(pressButtonCount + 1);
+  }
+  
+  const pressPrev = () => {
+    if(am.hasPrev()) {
+      setSlideNumber(slideNumber - 1);
+    }
+    am.prev();
+    // setPressButtonCount(pressButtonCount + 1);
   }
 
+  const pressReplay = () => { am.replay(); }
+  
   // tlp.addSwapAnimation('.sample-node .test1', '.sample-node .test3', 1000, 0, 0, 0, 0);
   // tlp.addSwapAnimation('.sample-node .test1', '.sample-node .test3', 1000, 0, 0, 0, 0);
   
   useEffect(() => {
     // strict modeだとuseEffectが2回呼ばれる
     if (inited) return;
-
-    console.log('useEffect');
-    tlp.addSwapAnimation('.sample-node .test1', '.sample-node .test2', 1000, 0, 0, 0, 0);
-    tlp.addSwapAnimation('.sample-node .test1', '.sample-node .test3', 1000, 0, 0, 0, 0);
-    tlp.addSwapAnimation('.sample-node .test1', '.sample-node .test4', 1000, 0, 0, 0, 0);
+    // setAM(() => createAM());
     
-    document.querySelector('.next').onclick = pressNext;
-    document.querySelector('.replay').onclick = tlp.play;
-    document.querySelector('.prev').onclick = tlp.prev;
+    console.log('useEffect');
+    // tlp.addSwapAnimation('.sample-node .test1', '.sample-node .test2', 1000, 0, 0, 0, 0);
+    // tlp.addSwapAnimation('.sample-node .test1', '.sample-node .test3', 1000, 0, 0, 0, 0);
+    // tlp.addSwapAnimation('.sample-node .test1', '.sample-node .test4', 1000, 0, 0, 0, 0);
+    
     document.querySelector('.test').onclick = () => {
+      setPressButtonCount(pressButtonCount + 1);
       // sp.swapElementWithAnimation('.sample-node .test1', '.sample-node .test2');
     }
     inited = true;
   }, []);
 
   useEffect(() => {
-    setSlideNumber(tlp.timelineCursor);
+    // setSlideNumber(tlp.timelineCursor);
   }, [pressButtonCount]);
 
   return (
@@ -61,10 +90,13 @@ export default function Home() {
           <h1>{slideNumber}</h1>
         </div>
         <div className="slide-control">
-          <button className="prev   p-1 m-1 border">Prev</button>
-          <button className="replay p-1 m-1 border">Replay</button>
-          <button className="next   p-1 m-1 border">Next</button>
-          <button className="test   p-1 m-1 border">Test</button>
+          <button className="prev p-1 m-1 border"
+            onClick={pressPrev}>Prev</button>
+          <button className="replay p-1 m-1 border"
+            onClick={pressReplay}>Replay</button>
+          <button className="next p-1 m-1 border"
+            onClick={pressNext}>Next</button>
+          <button className="test p-1 m-1 border">Test</button>
         </div>
       </div>
     </main>
